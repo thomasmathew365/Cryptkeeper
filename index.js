c8adf6aa739715db0b36fbaaab5d7dcf7cb2055e
ashrayvenkat <ashray.venkat102@gmail.com>
2018-03-17 16:48:21 +0530

    https request to api is setup

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
 
+const https = require('https');
+
+
+
+const url =
+  "https://api.coinmarketcap.com/v1/ticker/";
+
 const port = 3000;
 
+app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('static'));
+
 app.get('/',function (req, res) {
-  res.send("Hello, World!");
+  res.sendfile('./views/index.html');
+});
+
+app.get('/all',function (req, res) {
+
+  https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
+    res.on("end", () => {
+      body = JSON.parse(body);
+      console.log(body);
+    });
+  });
+
 });
 
 
